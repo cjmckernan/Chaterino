f9b756cc195cf91aa0b332dacf636dcb68b051b8
cjmckernan <conormckernan1992@gmail.com>
2017-03-15 17:06:18 +0000

    Added parallax. Will add images and content then login later.

--- src/main/java/com/conor/chaterino/SpringApp.java
+++ src/main/java/com/conor/chaterino/SpringApp.java
@@ -14,7 +14,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class SpringApp {
-    
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class, args);
     }
